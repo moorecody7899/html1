133ab96c75d4919a57defb0fa6ea539855e63d12
moorecody7899 <94501773+moorecody7899@users.noreply.github.com>
2022-02-08 23:14:32 -0700

    Update script.js
    added clock with greeting.

--- ResumeFolder/Resume/script.js
+++ ResumeFolder/Resume/script.js
@@ -1,6 +1,26 @@
 
 function askName() {
     const firstName = prompt("what is your name?");
-    alert (firstName + " , thank you.");
+    //alert (firstName + " , thank you.");
+  
 
+
+    
+
+
+    (function (){
+        var date = new Date();
+        console.log(date);
+        var hours = date.getHours();
+        console.log(hours);
+    
+        if (hours >=5 && hours <= 12) {
+            alert ("Good morning," + firstName + "! Thank you for reviewing my resume site.");
+        } else if (hours > 12 && hours <= 18){
+            alert( "Good afternoon, " + firstName +  "! Thank you for reviewing my resume site.");
+        } else if (hours > 18){
+            alert(" Good evening, " + firstName +  "! Thank you for reviewing my resume site.")}}
+        
+    )();   
 }
+//“Good Morning, (Name they Entered)! Thank you for reviewing my resume site.”
