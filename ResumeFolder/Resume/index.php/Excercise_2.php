c39c7069136543de4cff6c6743d1772c8559a0d2
moorecody7899 <94501773+moorecody7899@users.noreply.github.com>
2022-04-22 20:20:07 -0600

    php
    php

--- ResumeFolder/Resume/index.php/Excercise_2.php
+++ ResumeFolder/Resume/index.php/Excercise_2.php
@@ -8,6 +8,75 @@
 <body>
 <?php
 echo 'Hello World';
+
+     
+      
+        $x = 2;
+        $y = 6;
+
+      
+        echo $x = $y;
+        echo "<br>";
+
+      
+        echo $x += $y;
+        echo "<br>";
+
+    
+        echo $x -= $y;
+        echo "<br>";
+
+       
+        echo $x *= $y;
+        echo "<br>";
+
+    
+        echo $x /= $y;
+        echo "<br>";
+
+      
+        echo $x %= $y;
+        echo "<br>";
+        
+        echo 'The value of $x is equal to $y: ';
+        echo var_dump($x == $y);
+        echo "<br>";
+       
+        echo 'The value of $x is not equal to $y: ';
+        echo var_dump($x != $y);
+        echo "<br>";
+        
+        echo '$x is identical to $y: ';
+        echo var_dump($x === $y);
+        echo "<br>";
+        
+        echo '$x is not identical to $y: ';
+        echo var_dump($x !== $y);
+        echo "<br>";
+        
+        echo '$x is greater than $y: ';
+        echo var_dump($x > $y);
+        echo "<br>";
+        
+         echo '$x is less than $y: ';
+         echo var_dump($x < $y);
+         echo "<br>";
+         
+          echo '$x is greater than or equal to $y: ';
+          echo var_dump($x >= $y);
+          echo "<br>";
+          
+           echo '$x is less than or equal to $y: ';
+           echo var_dump($x <= $y);
+           echo "<br>";
+	
+
+
+
+
+
+
+
 ?>
     
 </body>
