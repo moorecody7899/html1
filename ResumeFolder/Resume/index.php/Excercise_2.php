fcd4191dbe8a5ef712060f4940b46e908b322f48
moorecody7899 <94501773+moorecody7899@users.noreply.github.com>
2022-04-24 15:03:36 -0600

    php
    working on php

--- ResumeFolder/Resume/index.php/Excercise_2.php
+++ ResumeFolder/Resume/index.php/Excercise_2.php
@@ -8,6 +8,10 @@
 <body>
 <?php
 echo 'Hello World';
+echo "<br>";
+
+
+
 
      
       
@@ -69,6 +73,35 @@ echo 'Hello World';
            echo '$x is less than or equal to $y: ';
            echo var_dump($x <= $y);
            echo "<br>";
+           echo "<br>";
+           $newVar1 = "I love burgers at lucky13!<br>";
+           echo $newVar1;
+           echo "<br>";
+           $newVar2 = "lobster pasta is the best!<br>";
+           echo $newVar2;
+           $varString1 = "I love Beers! <br>";
+           echo "<br>";
+           $varString2 = "I love tacos <br>";
+           echo "<br>";
+           echo $varString1;
+           echo "<br>";
+           echo $varString2;
+           echo "<br>";
+           echo "<br>";
+           
+           $varArray = [ 'Tacos', 'Burgers', 'Lobster', 'Beer', ];
+           print_r ($varArray); 
+
+           echo "<br>";
+
+           Echo "This ". "was ". " a ". "fun ". "string! ";
+
+           $Date = date("M. d, Y");
+           echo "Anyways this is". $Date.  "And now im hungry!";
+           echo str_replace('love', 'Hate', 'I love Burger the Most!'); echo "<br>";
+           echo "<br>";
+           echo str_word_count('I love Burger the Most!'); echo "<br>";
+          
 	
 
 
